f1db40efc2962ea4ab2865ae4bf3ed070a157fef
Rudy Suyanto <free4spams@yahoo.com>
2012-04-06 16:50:42 +0700

    first commit

---
+++ src/com/rudsu/tipcalculator/TipCalculator.java
@@ -0,0 +1,19 @@
+package com.rudsu.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class TipCalculator extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
